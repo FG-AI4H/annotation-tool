8b07315f2d9101273924a13d555bf6dedd365c1e
Marc Lecoultre <ml@mllab.ai>
2022-05-22 13:43:18 +0200

    Secret Manager

---
+++ src/main/java/org/fgai4h/ap/DataSourceConfig.java
@@ -0,0 +1,36 @@
+package org.fgai4h.ap;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+import static org.fgai4h.ap.helpers.SecretsManager.getSecret;
+
+@Configuration
+public class DataSourceConfig {
+
+        @Bean
+        public DataSource getDataSource() {
+                DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+                dataSourceBuilder.driverClassName("com.mysql.jdbc.Driver");
+                dataSourceBuilder.url("jdbc:mysql://fg-ai4h-db.cluster-cllejqtoj5fh.eu-central-1.rds.amazonaws.com:3306/fg_ai4h");
+                String secret = getSecret();
+                ObjectMapper objectMapper  =  new  ObjectMapper();
+                JsonNode secretsJson  = null;
+                try {
+                        secretsJson = objectMapper.readTree(secret);
+                        dataSourceBuilder.username(secretsJson.get("username").textValue());
+                        dataSourceBuilder.password(secretsJson.get("password").textValue());
+
+                } catch (JsonProcessingException e) {
+                        e.printStackTrace();
+                }
+
+                return dataSourceBuilder.build();
+        }
+}
--- src/main/java/org/fgai4h/ap/helpers/SecretsManager.java
+++ src/main/java/org/fgai4h/ap/helpers/SecretsManager.java
@@ -1,28 +1,73 @@
 package org.fgai4h.ap.helpers;
 
+import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.secretsmanager.SecretsManagerClient;
-import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueRequest;
-import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueResponse;
-import software.amazon.awssdk.services.secretsmanager.model.SecretsManagerException;
+import software.amazon.awssdk.services.secretsmanager.model.*;
+
+import java.util.Base64;
 
 public class SecretsManager {
 
-    static SecretsManagerClient secretsClient = SecretsManagerClient.builder().build();
+    // Use this code snippet in your app.
+// If you need more information about configurations or implementing the sample code, visit the AWS docs:
+// https://docs.aws.amazon.com/sdk-for-java/v1/developer-guide/java-dg-samples.html#prerequisites
 
-    public static void getValue(String secretName) {
+    public static String getSecret() {
+
+        String secretName = "arn:aws:secretsmanager:eu-central-1:601883093460:secret:annotation-backend/db-nzARqA";
+        Region region = Region.of("eu-central-1");
+
+        // Create a Secrets Manager client
+        SecretsManagerClient client = SecretsManagerClient.builder()
+                .region(region)
+                .build();
+
+        // In this sample we only handle the specific exceptions for the 'GetSecretValue' API.
+        // See https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+        // We rethrow the exception by default.
+
+        String secret = null, decodedBinarySecret;
+        GetSecretValueRequest getSecretValueRequest = GetSecretValueRequest.builder()
+                .secretId(secretName)
+                .build();
+        GetSecretValueResponse getSecretValueResponse = null;
 
         try {
-
-            GetSecretValueRequest valueRequest = GetSecretValueRequest.builder()
-                    .secretId(secretName)
-                    .build();
-
-            GetSecretValueResponse valueResponse = secretsClient.getSecretValue(valueRequest);
-            String secret = valueResponse.secretString();
-            System.out.println(secret);
-
-        } catch (SecretsManagerException e) {
-            System.err.println(e.awsErrorDetails().errorMessage());
+            getSecretValueResponse = client.getSecretValue(getSecretValueRequest);
+        } catch (DecryptionFailureException e) {
+            // Secrets Manager can't decrypt the protected secret text using the provided KMS key.
+            // Deal with the exception here, and/or rethrow at your discretion.
+            throw e;
+        } catch (InternalServiceErrorException e) {
+            // An error occurred on the server side.
+            // Deal with the exception here, and/or rethrow at your discretion.
+            throw e;
+        } catch (InvalidParameterException e) {
+            // You provided an invalid value for a parameter.
+            // Deal with the exception here, and/or rethrow at your discretion.
+            throw e;
+        } catch (InvalidRequestException e) {
+            // You provided a parameter value that is not valid for the current state of the resource.
+            // Deal with the exception here, and/or rethrow at your discretion.
+            throw e;
+        } catch (ResourceNotFoundException e) {
+            // We can't find the resource that you asked for.
+            // Deal with the exception here, and/or rethrow at your discretion.
+            throw e;
         }
+
+        // Decrypts secret using the associated KMS key.
+        // Depending on whether the secret is a string or binary, one of these fields will be populated.
+        if (getSecretValueResponse.secretString() != null) {
+            secret = getSecretValueResponse.secretString();
+        }
+        else {
+            decodedBinarySecret = new String(Base64.getDecoder().decode(getSecretValueResponse.secretBinary().asByteBuffer()).array());
+        }
+
+        // Your code goes here.
+        return secret;
     }
+
+    private SecretsManager() {}
 }
