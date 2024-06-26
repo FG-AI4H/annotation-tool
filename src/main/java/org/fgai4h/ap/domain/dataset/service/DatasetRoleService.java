87a6940deb40c6976f66ab557f6a3bbeb57187d1
mlecoultre <ml@mllab.ai>
2024-06-26 19:06:37 +0200

    feat: add datasetRole endpoints

--- src/main/java/org/fgai4h/ap/domain/dataset/service/DatasetRoleService.java
+++ src/main/java/org/fgai4h/ap/domain/dataset/service/DatasetRoleService.java
@@ -2,13 +2,18 @@ package org.fgai4h.ap.domain.dataset.service;
 
 import lombok.RequiredArgsConstructor;
 import org.fgai4h.ap.domain.dataset.entity.DatasetRoleEntity;
+import org.fgai4h.ap.domain.dataset.mapper.DatasetRoleApiMapper;
 import org.fgai4h.ap.domain.dataset.mapper.DatasetRoleMapper;
 import org.fgai4h.ap.domain.dataset.mapper.DatasetRoleModelAssembler;
+import org.fgai4h.ap.domain.dataset.model.DatasetModel;
 import org.fgai4h.ap.domain.dataset.model.DatasetRoleModel;
 import org.fgai4h.ap.domain.dataset.repository.DatasetRoleRepository;
+import org.fgai4h.ap.domain.error.DomainError;
+import org.fgai4h.ap.domain.exception.NotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
@@ -26,6 +31,10 @@ public class DatasetRoleService {
         return datasetModelAssembler.toModel(datasetRoleEntity);
     }
 
+    public void updateDatasetRole(DatasetRoleModel datasetRoleModel) {
+        datasetRoleRepository.save(datasetRoleMapper.toDatasetRoleEntity(datasetRoleModel));
+    }
+
     public void deleteRolesByDatasetId(UUID datasetId) {
         List<DatasetRoleEntity> roles = datasetRoleRepository.findRolesByDatasetId(datasetId);
         roles.stream().forEach(r -> datasetRoleRepository.deleteById(r.getDatasetRoleUUID()));
@@ -33,5 +42,16 @@ public class DatasetRoleService {
 
     public List<DatasetRoleModel> getDatasetRolesForDatasetId(UUID datasetId){
         return datasetRoleRepository.findRolesByDatasetId(datasetId).stream().map(datasetRoleModelAssembler::toModel).collect(Collectors.toList());
+    }
+
+    public void deleteDatasetRoleById(UUID datasetRoleId) {
+        datasetRoleRepository.deleteById(datasetRoleId);
+    }
+
+    public Optional<DatasetRoleModel> getDatasetRoleById(UUID datasetRoleId) {
+        return Optional.ofNullable(datasetRoleRepository
+                .findById(datasetRoleId)
+                .map(datasetRoleModelAssembler::toModel)
+                .orElseThrow(() -> new NotFoundException(DomainError.NOT_FOUND, "DatasetRole", "id", datasetRoleId)));
     }
 }
